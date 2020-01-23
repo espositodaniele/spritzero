041d5b1d02f8da1886d400dea62fd428be01f62c
Daniele Esposito <danyesp5@gmail.com>
2020-01-23 17:41:19 +0100

    link favico corretto

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/assets/favicon.ico" />
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body class="bg-10">
